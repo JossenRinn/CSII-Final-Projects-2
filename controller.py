eee953119a3427ff379bd360e4be298679e12a9e
JossenRinn <108024321+JossenRinn@users.noreply.github.com>
2022-12-02 16:50:47 -0600

    Add files via upload

---
+++ controller.py
@@ -0,0 +1,10 @@
+from PyQt5.QtWidgets import *
+from CS2Project2 import *
+
+QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
+QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
+
+class Controller(QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setupUi(self)
